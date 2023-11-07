b705970fdae3596c91f8416e79cba0c3323bcfbc
Miguel Ferrer <mferrer.ieu2021@student.ie.edu>
2023-11-07 17:32:26 +0100

    commit

--- table_classes.py
+++ table_classes.py
@@ -1,6 +1,12 @@
 import threading
 import random
 import time
+import names
+import random
 
 class Table:
-    
+    def __init__(self):
+        self.Name = names.get_first_name()
+        self.Age = random.randrange(18, 60)
+
+    def deal_cards():
