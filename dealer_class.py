7ef3c5499d041cbc77fe343d2f586b9eac4ecc99
Miguel Ferrer <mferrer.ieu2021@student.ie.edu>
2023-11-07 19:51:44 +0100

    add lock to dealer

--- dealer_class.py
+++ dealer_class.py
@@ -9,6 +9,7 @@ class Dealer(Deck):
     def __init__(self) -> None:
         self.Name = names.get_first_name()
         self.Age = random.randrange(18, 60)
+        self.Lock = threading.Lock()
 
     # Inherits functions:
     # - shuffle_deck()
