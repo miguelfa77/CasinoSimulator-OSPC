50f4db2c33b5ef4208f306c8acd20f704871f262
almupoveda <119288202+almupoveda@users.noreply.github.com>
2023-11-07 18:24:36 +0100

    Update bartender_class.py

--- bartender_class.py
+++ bartender_class.py
@@ -3,5 +3,5 @@ import names
 
 class Bartender:
     def __init__(self):
-        self.name = name.get_first_name()
+        self.name = names.get_first_name()
         self.age = random.randint(18, 60)
