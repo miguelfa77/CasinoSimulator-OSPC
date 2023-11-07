7c7e8adde609f3e32cdac111b55fee57ae268f43
Miguel Ferrer <mferrer.ieu2021@student.ie.edu>
2023-11-07 19:45:09 +0100

    import deck in dealer

--- dealer_class.py
+++ dealer_class.py
@@ -1,1 +1,15 @@
+import threading
+import random
+import time
+import names
+import random
+from deck_class import Deck
 
+class Dealer(Deck):
+    def __init__(self) -> None:
+        self.Name = names.get_first_name()
+        self.Age = random.randrange(18, 60)
+
+    # Inherits functions:
+    # - shuffle_deck()
+    # - draw_card()
--- table_classes.py
+++ table_classes.py
@@ -1,14 +1,3 @@
-import threading
-import random
-import time
-import names
-import random
-
-class Dealer(Deck):
-    def __init__(self) -> None:
-        self.Name = names.get_first_name()
-        self.Age = random.randrange(18, 60)
-
 
 
 
