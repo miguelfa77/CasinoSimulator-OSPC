650c338d8558debaa27056d844051e546cd5940a
Miguel Ferrer <mferrer.ieu2021@student.ie.edu>
2023-11-07 21:17:04 +0100

    latest

--- dealer_class.py
+++ dealer_class.py
@@ -6,9 +6,10 @@ import random
 from deck_class import Deck
 
 class Dealer(Deck):
-    def __init__(self) -> None:
+    def __init__(self, id) -> None:
+        self.dealer_id = id
         self.Name = names.get_first_name()
-        self.Age = random.randrange(18, 60)
+        self.Age = random.randint(18, 60)
         self.Lock = threading.Lock()
 
     # Inherits functions:
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import threading
 import time
 import random
 import numpy as np
+import concurrent.futures
+
+
 
 
 
