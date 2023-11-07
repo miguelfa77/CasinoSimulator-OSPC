19ca2106d093970bf380a3a76b332b26301ea456
Miguel Ferrer <mferrer.ieu2021@student.ie.edu>
2023-11-07 21:17:10 +0100

    latest

--- dealer_class.py
+++ dealer_class.py
@@ -4,6 +4,7 @@ import time
 import names
 import random
 from deck_class import Deck
+from table_classes import Table
 
 class Dealer(Deck):
     def __init__(self, id) -> None:
@@ -15,3 +16,16 @@ class Dealer(Deck):
     # Inherits functions:
     # - shuffle_deck()
     # - draw_card()
+
+    def take_bets(self, customer_id, quantity):
+        current_bets[customer_id] =  quantity
+
+    def pay_winners(self, quantity, table : Table) -> int:
+        table.balance -= quantity
+        return table.balance
+        
+
+    def collect_losers(self, quantity, table : Table) -> int:
+        table.balance += quantity
+        return table.balance
+
