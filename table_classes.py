f3f498c3c60ac661dd33efbf7298f8127bd1a5d7
deecastroo <118441213+deecastroo@users.noreply.github.com>
2023-11-10 11:34:52 +0100

    Update table_classes.py

--- table_classes.py
+++ table_classes.py
@@ -1,3 +1,20 @@
+import random
+import threading 
+
+Class Tables(threading.Thread):
+
+        def __init__(self,balance):
+                super().__init__()
+                self.balance = balance 
+                print(f"The balance of the Casino is", balance)
+                self.lock = threading.Lock()
+        def run(self):
+                self.lock.acquire()
+                time.sleep(3)
+                self.lock.release()
+
+
+
 
 
 
