51b8a6ee67099bce53d0efd21d72a5bdc381c64d
Miguel Ferrer Alvarez <miguelfa@Miguels-MacBook-Air.local>
2023-11-25 18:49:37 +0100

    main.py update

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from bouncer_class import Bouncer
 from deck_class import NormalDeck, BlackJackDeck
 
 if __name__ == "__main__":
+    
     starting_balance = 10000000
     NUM_OF_TABLES = 10
     NUM_OF_CUSTOMERS = 100
@@ -19,17 +20,14 @@ if __name__ == "__main__":
     NUM_OF_BARTENDERS = 5
     NUM_OF_BOUNCERS = 2
 
-    casino = Casino(starting_balance)
+    casino = Casino(STARTING_BALANCE=starting_balance,
+                    NUM_OF_TABLES=NUM_OF_TABLES,
+                    NUM_OF_CUSTOMERS=NUM_OF_CUSTOMERS,
+                    NUM_OF_DEALERS=NUM_OF_DEALERS,
+                    NUM_OF_BARTENDERS=NUM_OF_BARTENDERS,
+                    NUM_OF_BOUNCERS=NUM_OF_BOUNCERS)
 
-    with concurrent.futures.ThreadPoolExecutor(num_workers=1) as exe:
-        exe.submit(casino.run)
-        with concurrent.futures.ThreadPoolExecutor(num_workers=range(num_of_tables)) as exe:
-            for table in tables:
-                exe.submit(table.run)
-            with concurrent.futures.ThreadPoolExecutor(num_workers=range(num_of_customers)) as exe1:
-                for customer in customers:
-                    exe.submit(customer.run)
-        
+    casino.run()
 
 
 
