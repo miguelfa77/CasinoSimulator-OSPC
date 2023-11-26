9a2a8cfa0a02fb8927816cd2da9d783af971844d
Esteban Sanchez <109927620+esanchezmex@users.noreply.github.com>
2023-11-26 21:26:47 +0100

    Fixed inheritance

--- main.py
+++ main.py
@@ -19,19 +19,24 @@ if __name__ == "__main__":
     num_of_bartenders = 5
     num_of_bouncers = 2
 
-    customers = [Customer() for _ in range(num_of_customers)]
+    casino = Casino(starting_balance)
+
+    high_spenders = [HighSpender(i, casino) for i in range(num_of_customers*(num_of_customers*0.05))]
+    medium_spenders = [MediumSpender(i, casino) for i in range(num_of_customers*(num_of_customers*0.75))]
+    low_spenders = [LowSpender(i, casino) for i in range(num_of_customers*(num_of_customers*0.2))]
+    customers = high_spenders + medium_spenders + low_spenders
+
     tables = [(Blackjack(id, BlackJackDeck), Roulette(id+1), Poker(id+2, NormalDeck)) for id in range(0, num_of_tables, 2)]
     dealers = [Dealer(id, tables) for id in range(num_of_dealers)]
     bartenders = [Bartender(id) for id in range(num_of_bartenders)]
     bouncers = [Bouncer(id) for id in range(num_of_bouncers)]
-    casino = Casino(starting_balance, customers, tables, bartenders, bouncers)
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=len(range(num_of_tables))) as exe:
         for table in tables:
             exe.submit(table.run)
         with concurrent.futures.ThreadPoolExecutor(max_workers=len(range(num_of_customers))) as exe1:
             for customer in customers:
-                exe.submit(customer.run, casino)
+                exe.submit(customer.run)
         
 
 
