c0e9a981caa502c753ce4eceabcb46d37b49d16a
Miguel Ferrer Alvarez <miguelfa@Miguels-MacBook-Air.local>
2023-11-27 00:00:05 +0100

    classes file logic

--- classes_file.py
+++ classes_file.py
@@ -3,3 +3,14 @@ from dealer_class import Dealer
 from customer_classes import Customer, HighRoller, MediumRoller, LowRoller, customer_type
 from bartender_class import Bartender
 from bouncer_class import Bouncer
+
+"""
+Logic to prevent circular imports:
+
+main.py -> Casino
+    Casino -> Roulette, Blackjack, Poker, Dealer, Bartender, Bouncer, customer_type
+        Bouncer -> Customer
+        Dealer -> Deck
+        Table -> deck_type
+
+"""
